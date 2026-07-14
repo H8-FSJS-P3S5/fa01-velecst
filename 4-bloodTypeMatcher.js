f9fc4b01f9174a9ba2ced10a23bd2f980dd365cb
vel <calistavelecia@gmail.com>
2026-07-14 14:37:32 +0700

    Soal 4 Done

--- 4-bloodTypeMatcher.js
+++ 4-bloodTypeMatcher.js
@@ -31,59 +31,75 @@ alasan: karena O dan A tidak menghasilkan anak bergolongan B
 */
 
 const bloodTypeTable = [
-    {
-        parents: ["A", "A"],
-        child: ["A", "O"],
-    },
-    {
-        parents: ["A", "B"],
-        child: ["A", "B", "O", "AB"],
-    },
-    {
-        parents: ["A", "O"],
-        child: ["A", "O"],
-    },
-    {
-        parents: ["A", "AB"],
-        child: ["A", "B", "AB"],
-    },
-    {
-        parents: ["B", "B"],
-        child: ["B", "O"],
-    },
-    {
-        parents: ["B", "O"],
-        child: ["B", "O"],
-    },
-    {
-        parents: ["B", "AB"],
-        child: ["A", "B", "AB"],
-    },
-    {
-        parents: ["O", "O"],
-        child: ["O"],
-    },
-    {
-        parents: ["O", "AB"],
-        child: ["A", "B"],
-    },
-    {
-        parents: ["AB", "AB"],
-        child: ["A", "B", "AB"],
-    },
+  {
+    parents: ["A", "A"],
+    child: ["A", "O"],
+  },
+  {
+    parents: ["A", "B"],
+    child: ["A", "B", "O", "AB"],
+  },
+  {
+    parents: ["A", "O"],
+    child: ["A", "O"],
+  },
+  {
+    parents: ["A", "AB"],
+    child: ["A", "B", "AB"],
+  },
+  {
+    parents: ["B", "B"],
+    child: ["B", "O"],
+  },
+  {
+    parents: ["B", "O"],
+    child: ["B", "O"],
+  },
+  {
+    parents: ["B", "AB"],
+    child: ["A", "B", "AB"],
+  },
+  {
+    parents: ["O", "O"],
+    child: ["O"],
+  },
+  {
+    parents: ["O", "AB"],
+    child: ["A", "B"],
+  },
+  {
+    parents: ["AB", "AB"],
+    child: ["A", "B", "AB"],
+  },
 ];
 
 function isMatchBloodType(parent1, parent2, children) {
-    // write your code here
+  for (let i = 0; i < bloodTypeTable.length; i++) {
+    let dad = bloodTypeTable[i].parents[0];
+    let mom = bloodTypeTable[i].parents[1];
 
+    if (
+      (dad === parent1 && mom === parent2) ||
+      (dad === parent2 && mom === parent1)
+    ) {
+      let chances = bloodTypeTable[i].child;
+
+      for (let j = 0; j < chances.length; j++) {
+        if (chances[j] === children) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  return false;
 }
-
 // TEST CASE
-console.log(isMatchBloodType('O', 'A', 'O')) // true
-console.log(isMatchBloodType('O', 'A', 'A')) // true
-console.log(isMatchBloodType('O', 'A', 'B')) // false
-console.log(isMatchBloodType('O', 'B', 'B')) // true
-console.log(isMatchBloodType('AB', 'B', 'O')) // false
-console.log(isMatchBloodType('AB', 'B', 'B')) // true
+console.log(isMatchBloodType("O", "A", "O")); // true
+console.log(isMatchBloodType("O", "A", "A")); // true
+console.log(isMatchBloodType("O", "A", "B")); // false
+console.log(isMatchBloodType("O", "B", "B")); // true
+console.log(isMatchBloodType("AB", "B", "O")); // false
+console.log(isMatchBloodType("AB", "B", "B")); // true
 
 module.exports = isMatchBloodType;
